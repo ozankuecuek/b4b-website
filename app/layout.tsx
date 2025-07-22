f6656ceaa946714f0f35193c5be36f6758b5501a
Ozan Kücük <ozan.kuecuek@diva-e.com>
2025-07-22 14:16:08 +0200

    font consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${lexend.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
